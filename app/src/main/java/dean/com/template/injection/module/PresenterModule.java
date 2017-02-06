8eff697f922e94b65ab5d9474debde5838e26a9a
Dean <dean.jakovljevic12@gmail.com>
2017-02-06 19:41:22 +0100

    error fix

--- app/src/main/java/dean/com/template/injection/module/PresenterModule.java
+++ app/src/main/java/dean/com/template/injection/module/PresenterModule.java
@@ -7,6 +7,7 @@ import dagger.Provides;
 import dean.com.template.data.api.converter.MovieAPIConverter;
 import dean.com.template.domain.usecase.MovieUseCase;
 import dean.com.template.injection.scope.ForActivity;
+import dean.com.template.manager.StringManager;
 import dean.com.template.ui.home.HomePresenter;
 import dean.com.template.ui.home.HomePresenterImpl;
 import io.reactivex.Scheduler;
@@ -21,8 +22,8 @@ public final class PresenterModule {
     @ForActivity
     @Provides
     HomePresenter provideHomePresenter(@Named(SUBSCRIBE_SCHEDULER) Scheduler subscribeScheduler,
-            @Named(OBSERVE_SCHEDULER) Scheduler observeScheduler, MovieUseCase movieUseCase, MovieAPIConverter movieAPIConverter) {
-        return new HomePresenterImpl(subscribeScheduler, observeScheduler, movieUseCase, movieAPIConverter);
+            @Named(OBSERVE_SCHEDULER) Scheduler observeScheduler, MovieUseCase movieUseCase, MovieAPIConverter movieAPIConverter, StringManager stringManager) {
+        return new HomePresenterImpl(subscribeScheduler, observeScheduler, movieUseCase, movieAPIConverter, stringManager);
     }
 
 }
