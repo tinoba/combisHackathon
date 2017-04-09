9c7f8a02f2fac5a96161698b72dc44fbad947ecc
tinoba <tinbalint@gmail.com>
2017-04-09 04:23:31 +0200

    design fix

--- app/src/main/java/combis/hackathon/ui/voice/VehicleAdapter.java
+++ app/src/main/java/combis/hackathon/ui/voice/VehicleAdapter.java
@@ -1,14 +1,17 @@
 package combis.hackathon.ui.voice;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -18,6 +21,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import combis.hackathon.R;
 import combis.hackathon.domain.model.Vehicle;
+import combis.hackathon.ui.home.HomeActivity;
 
 public class VehicleAdapter extends ArrayAdapter<Vehicle> {
 
@@ -42,6 +46,10 @@ public class VehicleAdapter extends ArrayAdapter<Vehicle> {
             view.setTag(holder);
         }
 
+        holder.imageButton.setOnClickListener(v -> {
+            getContext().startActivity(new Intent(getContext(), HomeActivity.class));
+            Toast.makeText(getContext(), "Transportation booked", Toast.LENGTH_SHORT).show();
+        });
         holder.agencijaName.setText(vehicles.get(position).agencyName);
         holder.cijena.setText(vehicles.get(position).cijena + " kn/km");
         holder.radnoVrijeme.setText(vehicles.get(position).radnoVrijeme);
@@ -62,6 +70,9 @@ public class VehicleAdapter extends ArrayAdapter<Vehicle> {
         TextView radnoVrijeme;
         @BindView(R.id.vehicle_image)
         ImageView vehicleImage;
+
+        @BindView(R.id.order_vehicle)
+        ImageButton imageButton;
 
         public ViewHolder(View view) {
             ButterKnife.bind(this, view);
